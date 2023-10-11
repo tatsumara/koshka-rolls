2c75290252ae00cd992e3239c21446d295e71c3e
tatsumara <mara@tatsumara.dev>
2023-10-11 16:29:45 +0200

    feat: get basic functionality working

---
+++ index.js
@@ -0,0 +1,31 @@
+function parseDiceRoll(input) {
+	// Remove spaces from the input.
+	input = input.replace(/\s/g, '');
+  
+	// Define a regular expression pattern to match the format "2d6+1" or "10d12-5".
+	const regex = /^(\d+)d(\d+)([+\-]\d+)?$/;
+  
+	// Use the regex pattern to match the input string.
+	const match = input.match(regex);
+  
+	if (match) {
+	  // Extract the matched values and convert them to numbers.
+	  const amount = parseInt(match[1]);
+	  const sides = parseInt(match[2]);
+	  
+	  // Extract the bonus part and convert it to a number.
+	  const bonusMatch = match[3];
+	  const bonus = bonusMatch ? parseInt(bonusMatch) : 0;
+  
+	  // Return an object with the parsed values.
+	  return { amount, sides, bonus };
+	} else {
+	  // Return null if the input doesn't match the expected format.
+	  return null;
+	}
+}
+
+console.log(parseDiceRoll('2d6+1'))
+console.log(parseDiceRoll('3d20-4'))
+console.log(parseDiceRoll('10d12-5'))
+console.log(parseDiceRoll('5d2'))
