c7c852b92be115e3d70a4937db45faae856d17f1
tatsumara <mara@tatsumara.dev>
2023-10-12 20:17:31 +0200

    feat: get basic bot working

--- index.js
+++ index.js
@@ -25,7 +25,36 @@ function parseDiceRoll(input) {
 	}
 }
 
-console.log(parseDiceRoll('2d6+1'))
-console.log(parseDiceRoll('3d20-4'))
-console.log(parseDiceRoll('10d12-5'))
-console.log(parseDiceRoll('5d2'))
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const intents = [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMembers,
+	GatewayIntentBits.GuildMessages
+];
+const client = new Client({
+	intents: intents,
+	presence: {
+		activities: [{
+			type: 'PLAYING',
+			name: 'with the yarn of fate.',
+		}],
+	},
+});
+client.log = require('consola');
+
+client.once(Events.ClientReady, c => {
+	client.log.success(`Ready! Logged in as ${c.user.tag}`)
+})
+
+process.on('unhandledRejection', error => {
+	client.log.error('Unhandled promise rejection:');
+	client.log.error(error);
+});
+process.on('uncaughtException', error => {
+	client.log.error('Uncaught exception:');
+	client.log.error(error);
+});
+
+client.login(process.env.TOKEN);
